69bdf51ca9d105e4bb89a3237c138ab143cabcc6
CatMoon <60377000+imshota1009@users.noreply.github.com>
2025-09-06 13:06:48 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'dino-rpg-runner-v1';
+// キャッシュするファイルのリスト
+const urlsToCache = [
+  '/',
+  'index.html',
+  'game.js',
+  'background_music.mp3',
+  'boss_music.mp3',
+  'dino_player.png',
+  // 注意: アイコン画像もキャッシュリストに追加
+  'images/icon-192.png',
+  'images/icon-512.png'
+];
+
+// インストール処理
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// ファイルのリクエスト時にキャッシュから返す
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // キャッシュ内にファイルがあればそれを返す
+        if (response) {
+          return response;
+        }
+        // なければネットワークから取得する
+        return fetch(event.request);
+      }
+    )
+  );
+});
